70456fb738ecd5ab3e2ce697448879448b85a02f
Ayshashifa <vaseemaysha@gmail.com>
2024-02-17 13:02:32 +0530

    landing page add shop producs

--- src/app/components/food/landing-page/landing-page.component.ts
+++ src/app/components/food/landing-page/landing-page.component.ts
@@ -4,24 +4,46 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
-
   news: any[] = [];
+  firstdata: any;
+  secdata: any;
+  thirddata: any;
+  shop: any[] = [];
+  firstshop:any;
+  secshop:any;
+  thirdshop: any;
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.getShop();
     this.getnews();
   }
   getnews(): void {
     this.http.get(appApiResources.getmoreNews).subscribe({
       next: (data: any) => {
         this.news = data;
+        this.firstdata = data[0];
+        this.secdata = data[1];
+        this.thirddata = data[2];
         console.log('news', this.news);
+        console.log('first', this.firstdata);
       },
       error: (error: any) => {
         console.error('Error fetching news:', error);
+      },
+    });
+  }
+  getShop() {
+    this.http.get(appApiResources.getShopList).subscribe({
+      next: (data: any) => {
+        this.shop = data.datas;
+        this.firstshop = data.datas[0];
+        this.secshop = data.datas[1];
+        this.thirdshop = data.datas[2];
+        console.log('shop', this.shop);
       },
     });
   }
