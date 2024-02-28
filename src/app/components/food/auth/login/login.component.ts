a59b4bf576a12f5c4942e8f0ce1ab626470bcc6c
Ayshashifa <vaseemaysha@gmail.com>
2024-02-28 23:11:21 +0530

    login successfully

--- src/app/components/food/auth/login/login.component.ts
+++ src/app/components/food/auth/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { appApiResources } from 'src/app/constants/app.constants';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginform!: FormGroup;
+  constructor(private http: HttpClient, public fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginform = this.fb.group({
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
   }
-
+  login() {
+    if (this.loginform.valid) {
+      const body = {
+        email: this.loginform.value.email,
+        phone: this.loginform.value.phone,
+      };
+      this.http.post(appApiResources.login, body).subscribe({
+        next: (data: any) => {},
+      });
+    }
+  }
 }
--- src/app/components/food/auth/sign-up/sign-up.component.ts
+++ src/app/components/food/auth/sign-up/sign-up.component.ts
@@ -9,37 +9,38 @@ import { appApiResources } from 'src/app/constants/app.constants';
 })
 export class SignUpComponent implements OnInit {
   newform!: FormGroup;
-  
+
   constructor(public fb: FormBuilder, private http: HttpClient) {}
   // email: ['', [Validators.required, Validators.email]],
   ngOnInit(): void {
     this.newform = this.fb.group({
-      name: ['',Validators.required],
-      email: ['',Validators.required],
-      password: ['',Validators.required],
-      phone: ['',Validators.required],
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      phone: ['', Validators.required],
     });
   }
   // ssmnn.vaseem03@hmail.com
   submitForm() {
-    if(this.newform.valid){
-    const body = {
-      name:this.newform.value.name,
-      email:this.newform.value.email,
-      phone:this.newform.value.phone,
-      password:this.newform.value.password
+    if (this.newform.valid) {
+      const body = {
+        name: this.newform.value.name,
+        email: this.newform.value.email,
+        phone: this.newform.value.phone,
+        password: this.newform.value.password,
+      };
+      this.http.post(appApiResources.signUp, body).subscribe({
+        next: (newUser: any) => {
+          if (newUser.status === true) {
+            alert('success' + newUser.message);
+          } else {
+            alert('fail' + newUser.message);
+          }
+        },
+        error: (newUser) => {
+          alert('Error: ' + newUser.message);
+        },
+      });
     }
-    this.http.post(appApiResources.signUp, body).subscribe({
-      next:(newUser: any) =>{
-        if(newUser.status=== true){
-          alert("success"+ newUser.message)
-        }else{
-          alert("fail"+newUser.message)
-        }
-      },
-      error: (newUser) => {
-        alert("Error: " + newUser.message); // Display generic error message for HTTP request errors
-      }
-    });
-  }}
+  }
 }
